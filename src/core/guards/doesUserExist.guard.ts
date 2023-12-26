5a7f9dcd60d0c0872fb1eede52313e5416aed8f9
super1shy <supershynj3@gmail.com>
2023-12-26 23:08:34 +0300

    added guards, pipes and validation

---
+++ src/core/guards/doesUserExist.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from 'src/modules/users/users.service';
+
+@Injectable()
+export class DoesUserExist implements CanActivate {
+  constructor(private readonly userService: UsersService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<any> | Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(request) {
+    console.log('GUARD==============', request.body.email);
+
+    const userExist = await this.userService.findOneByEmail(request.body.email);
+    if (userExist) {
+      return new ForbiddenException('This email already exist');
+    }
+    return true;
+  }
+}
